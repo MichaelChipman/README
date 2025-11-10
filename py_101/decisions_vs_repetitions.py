7cf5971d3113df58250350b4dddf8c30623393ef
MichaelChipman <darkraven.mc@gmail.com>
2025-11-09 19:16:57 -0500

    eat: Add decisions_vs_repetitions.py (Lesson 16)
    Introduces the Lesson 16 file, marking the start of Week 2 material.
    
    The file serves as a conceptual introduction, explicitly differentiating between:
    * Selection structures (Decisions, denoted by #S).
    * Iteration structures (Repetitions, denoted by #R).

---
+++ py_101/decisions_vs_repetitions.py
@@ -0,0 +1,15 @@
+'''
+decisions_vs_repetitions.py
+Lesson: 1 Week 2: Decisions vs Repititions
+date 2025-11-9
+Author: Michael Chipman
+Learn decisions and repetition
+'''
+# One clear action per line, tags (# S / # R) aligned, tidy wording, even spacing.
+
+# Ask friends over to watch movie if friends are coming prepare seats #S
+# count number of friends #R
+# if seats < snacks_in_fridge, get more snacks #S
+# if everyone ready == true, start movie #S
+
+# end decisions_vs_repetitions.py
